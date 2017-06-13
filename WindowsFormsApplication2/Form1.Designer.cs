74ed34fc337e7e42d5170f7b0c3fb9ef700052b7
Hernan Zaldivar <hzaldivar@mylittlebets.com>
2017-06-13 14:20:15 -0300

    POC
    Taks POC

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblWhenAll = new System.Windows.Forms.Label();
+            this.lblSeparado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 48);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(128, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Esperar con WhenAll";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(39, 118);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(194, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Esperar cada tarea por separado";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(72, 87);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Tiempo";
+            // 
+            // lblWhenAll
+            // 
+            this.lblWhenAll.AutoSize = true;
+            this.lblWhenAll.Location = new System.Drawing.Point(113, 87);
+            this.lblWhenAll.Name = "lblWhenAll";
+            this.lblWhenAll.Size = new System.Drawing.Size(0, 13);
+            this.lblWhenAll.TabIndex = 3;
+            // 
+            // lblSeparado
+            // 
+            this.lblSeparado.AutoSize = true;
+            this.lblSeparado.Location = new System.Drawing.Point(113, 155);
+            this.lblSeparado.Name = "lblSeparado";
+            this.lblSeparado.Size = new System.Drawing.Size(0, 13);
+            this.lblSeparado.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(72, 155);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(42, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Tiempo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.lblSeparado);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblWhenAll);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblWhenAll;
+        private System.Windows.Forms.Label lblSeparado;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ WindowsFormsApplication2/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        Task<string> DoLongThingAsyncEx1()
+        {
+            return Task.FromException<string>(new Exception());
+        }
+
+        async Task<string> DoLongThingAsync(DateTime now)
+        {
+            await Task.Delay(5000);
+
+            //return Task.FromResult(DateTime.Now.ToString());
+            return now.ToString();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            var inicio = DateTime.Now;
+
+            //var tasks = this.GetTaskWithExceptions();
+            var tasks = this.GetTasks();
+
+            var task = Task.WhenAll(tasks);
+
+            var messageEx = string.Empty;
+
+            try
+            {
+                await task;
+            }
+            catch
+            {
+                messageEx = string.Join(", ", task.Exception.Flatten().InnerExceptions.Select(ex => ex.Message));
+            }
+
+            var totalSegundos = (DateTime.Now - inicio).TotalMilliseconds;
+
+            var resultados = string.Join("|", tasks.Where(t => !t.IsFaulted).Select(t => t.Result));
+
+            this.lblWhenAll.Text = totalSegundos.ToString();
+
+            MessageBox.Show($"Resultados: {resultados} - Exception {messageEx}");
+        }
+
+        private List<Task<string>> GetTasks()
+        {
+            var tasks = new List<Task<string>>();
+            
+            for (int i = 0; i < 10; i++)
+            {
+                tasks.Add(this.DoLongThingAsync(DateTime.Now));
+            }
+            return tasks;
+        }
+
+        private List<Task<string>> GetTaskWithExceptions()
+        {
+            var tasks = new List<Task<string>>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                tasks.Add(this.DoLongThingAsync(DateTime.Now));
+            }
+
+            tasks.Add(this.DoLongThingAsyncEx1());
+
+            for (int i = 0; i < 5; i++)
+            {
+                tasks.Add(this.DoLongThingAsync(DateTime.Now));
+            }
+            return tasks;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            var inicio = DateTime.Now;
+            var resultadolist = new List<string>();
+            var messageEx = string.Empty;
+
+            for (int i = 0; i < 10; i++)
+            {
+                try
+                {
+                    resultadolist.Add(await this.DoLongThingAsync(DateTime.Now));
+                }
+                catch (Exception exception)
+                {
+                    messageEx = exception.Message;
+                }
+            }
+
+            var totalSegundos = (DateTime.Now - inicio).TotalMilliseconds;
+
+            var resultados = string.Join("|", resultadolist);
+
+            this.lblSeparado.Text = totalSegundos.ToString();
+
+            MessageBox.Show($"Resultados: {resultados} - Exception {messageEx}");
+        }
+    }
+}
